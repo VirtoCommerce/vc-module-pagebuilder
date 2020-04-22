63d301976bf0c1df4ba0296bed433cc4efc224f4
basilkot <basilkot@gmail.com>
2020-04-22 10:36:01 +0200

    Add ability to share settings for different block types
    Some controls can have the same properties. Such properties can be extracted to separate settings in block_settings.json. Then some properties can be excluded. Also there is an ability to create named shared properties collections and include them to the block settings

--- cms-designer/src/app/modules/editor/services/blocks.service.ts
+++ cms-designer/src/app/modules/editor/services/blocks.service.ts
@@ -1,22 +1,68 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { PlatformService } from '@app/services';
-import { BlocksSchema } from '@shared/models';
+import { tap, map } from 'rxjs/operators';
+import { PlatformService, ApiUrlsService } from '@app/services';
+import { BlocksSchema, BlockSchema, SharedBlockSchema, ControlDescriptor } from '@shared/models';
 
 @Injectable({
     providedIn: 'root'
 })
 export class BlocksService {
-    constructor(private platform: PlatformService) { }
+    constructor(private platform: PlatformService, private urls: ApiUrlsService) { }
 
     load(): Observable<BlocksSchema> {
         return this.platform.downloadBlocksSchema().pipe(
             tap(schema => {
+                const shared = <SharedBlockSchema>schema.shared;
                 Object.keys(schema).forEach(key => {
-                    schema[key].type = key;
+                    if (!!schema[key].contentType && schema[key].contentType !== this.urls.params.contentType) {
+                        delete schema[key];
+                    } else {
+                        const currentBlock = schema[key];
+                        currentBlock.type = key;
+                        this.mergeSettings(currentBlock, shared);
+                    }
                 });
             })
         );
+    }
+
+    private mergeSettings(block: BlockSchema, shared: SharedBlockSchema) {
+        const blockSettings = block.settings;
+        // finction to add setting to block settings collection
+        const addSetting = (sharedSettings: ControlDescriptor[], excludePredicate: (string) => boolean = () => false) => {
+            if (!sharedSettings) return;
+            sharedSettings.forEach(item => {
+                if (!excludePredicate(item.id)) {
+                    const settingIndex = blockSettings.findIndex(b => b.id === item.id);
+                    if (settingIndex === -1) {
+                        blockSettings.push(item);
+                    } else {
+                        // some of properties can be overriden
+                        const overrides = blockSettings[settingIndex];
+                        blockSettings[settingIndex] = { ...item, ...overrides };
+                    }
+                }
+            });
+        };
+        // block.includeShared contains name or array of names named settings in shared block
+        if (block.includeShared) {
+            if (typeof block.includeShared === 'string') {
+                const sharedSettings = shared.namedSettings[block.includeShared];
+                addSetting(sharedSettings);
+            } else {
+                block.includeShared.forEach(name => {
+                    const sharedSettings = shared.namedSettings[name];
+                    addSetting(sharedSettings);
+                });
+            }
+        }
+        // block can skip shared settings or some of them
+        const skipShared = block.excludeShared;
+        if (!skipShared || Array.isArray(skipShared)) {
+            const exclude = Array.isArray(skipShared) ? (v: string) => skipShared.indexOf(v) !== -1 : () => false;
+            const sharedSettings = shared.settings;
+            addSetting(sharedSettings, exclude);
+        }
     }
 }
