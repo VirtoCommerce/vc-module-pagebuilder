5ff5b531b47455b74158b7aaeef2fca92d77eccc
basilkot <basilkot@gmail.com>
2020-04-30 13:24:52 +0200

    display block name as type by default

--- cms-designer/src/app/modules/editor/components/page-editor/page-editor.component.ts
+++ cms-designer/src/app/modules/editor/components/page-editor/page-editor.component.ts
@@ -74,7 +74,7 @@ export class PageEditorComponent implements OnInit {
             }
         }
         const unknownBlock = item;
-        result = result || unknownBlock.name || unknownBlock.title || unknownBlock.type || '<unnamed block>';
+        result = result || unknownBlock.name || unknownBlock.type || '<unnamed block>';
         if (result.length > 15) {
             result = result.substring(0, 15) + '…';
         }
