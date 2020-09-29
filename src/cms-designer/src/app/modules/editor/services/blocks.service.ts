9f101947562fddc318f032228220b5ea6a8450fa
basilkot <basilkot@gmail.com>
2020-09-29 09:47:39 +0200

    sort controls in block editor

--- src/cms-designer/src/app/modules/editor/services/blocks.service.ts
+++ src/cms-designer/src/app/modules/editor/services/blocks.service.ts
@@ -33,90 +33,47 @@ export class BlocksService {
 
     private mergeSettings(block: BlockSchema, shared: SharedBlockSchema) {
 
-        if (block.static || !shared)
-            return;
+        if (!block.static && shared) {
 
-        // 1. take all included shared settings
-        const sharedSettings = block.excludeShared === true ? [] : (!shared.settings ? [] : [...shared.settings]);
+            // 1. take all included shared settings
+            const sharedSettings = block.excludeShared === true ? [] : (!shared.settings ? [] : [...shared.settings]);
 
-        // 2. add included named shared
-        if (!!block.includeShared && shared.namedSettings) {
-            const sharedNames = typeof block.includeShared === 'string' ? [ block.includeShared ] : block.includeShared;
-            const sharedNamesSettings = sharedNames.filter(x => !!shared.namedSettings[x]).reduce((acc, name) => ([...acc, ...shared.namedSettings[name]]), []);
-            sharedSettings.push(...sharedNamesSettings);
-        }
+            // 2. add included named shared
+            if (!!block.includeShared && shared.namedSettings) {
+                const sharedNames = typeof block.includeShared === 'string' ? [block.includeShared] : block.includeShared;
+                const sharedNamesSettings = sharedNames.filter(x => !!shared.namedSettings[x]).reduce((acc, name) => ([...acc, ...shared.namedSettings[name]]), []);
+                sharedSettings.push(...sharedNamesSettings);
+            }
 
-        // 3. remove items from excluded list
-        if (Array.isArray(block.excludeShared)) {
-            block.excludeShared.forEach(id => {
-                const index = sharedSettings.findIndex(b => b.id === id);
-                if (index !== -1) {
-                    sharedSettings.splice(index, 1);
+            // 3. remove items from excluded list
+            if (Array.isArray(block.excludeShared)) {
+                block.excludeShared.forEach(id => {
+                    const index = sharedSettings.findIndex(b => b.id === id);
+                    if (index !== -1) {
+                        sharedSettings.splice(index, 1);
+                    }
+                });
+            }
+
+            // 4. override shared settings by block settings
+            if (!block.settings) {
+                block.settings = [];
+            }
+            const blockSettings = block.settings.map(x => {
+                const settings = sharedSettings.find(shared => shared.id == x.id);
+                if (!!settings) {
+                    return { ...settings, ...x };
+                } else {
+                    return x;
                 }
             });
+
+            // 5. apply items
+            const settings = [...blockSettings, ...sharedSettings.filter(x => !blockSettings.find(b => b.id === x.id))];
+            block.settings = settings;
         }
-        
-        // 4. override shared settings by block settings
-        if (!block.settings) {
-            block.settings = [];
-        }
-        const blockSettings = block.settings.map(x => {
-            const settings = sharedSettings.find(shared => shared.id == x.id);
-            if (!!settings) {
-                return {...settings, ...x};
-            } else {
-                return x;
-            }
-        });
-        sharedSettings.forEach(shared => {
-            const blockSettings = block.settings.find(x => x.id == shared.id);
-            if (!!blockSettings) {
-                
-            }
-        });
 
-        // 5. order items
-        const settings = [...blockSettings, ...sharedSettings.filter(x => !blockSettings.find(b => b.id === x.id))];
-        settings.sort((x, y) => x.sort - y.sort);
-        block.settings = settings;
-
-
-        // const blockSettings = block.settings;
-        // // finction to add setting to block settings collection
-        // const addSetting = (sharedSettings: ControlDescriptor[], excludePredicate: (string) => boolean = () => false) => {
-        //     if (!sharedSettings) return;
-        //     sharedSettings.forEach(item => {
-        //         if (!excludePredicate(item.id)) {
-        //             const settingIndex = blockSettings.findIndex(b => b.id === item.id);
-        //             if (settingIndex === -1) {
-        //                 blockSettings.push(item);
-        //             } else {
-        //                 // some of properties can be overriden
-        //                 const overrides = blockSettings[settingIndex];
-        //                 blockSettings[settingIndex] = { ...item, ...overrides };
-        //             }
-        //         }
-        //     });
-        // };
-        // // block.includeShared contains name or array of names named settings in shared block
-        // if (block.includeShared && shared.namedSettings) {
-        //     if (typeof block.includeShared === 'string') {
-        //         const sharedSettings = shared.namedSettings[block.includeShared];
-        //         addSetting(sharedSettings);
-        //     } else {
-        //         block.includeShared.forEach(name => {
-        //             const sharedSettings = shared.namedSettings[name];
-        //             addSetting(sharedSettings);
-        //         });
-        //     }
-        // }
-        // // block can skip shared settings or some of them
-        // const skipShared = block.excludeShared;
-        // if (!skipShared || Array.isArray(skipShared)) {
-        //     const exclude = Array.isArray(skipShared) ? (v: string) => (<string[]>skipShared).indexOf(v) !== -1 : () => false;
-        //     const sharedSettings = shared.settings;
-        //     addSetting(sharedSettings, exclude);
-        // }
-        // block.settings = [...block.settings.sort(b => b.sort)];
+        // 6. order items
+        block.settings.sort((x, y) => (x.sort || 0) - (y.sort || 0));
     }
 }
