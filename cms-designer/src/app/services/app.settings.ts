c8f0ecd4f337e2b36a847d2754274b04bfa9d294
Sergey Podguzov <spo@virtoway.com>
2021-02-05 15:19:49 +0500

    Added concrete path to module location in AppSettings.

--- cms-designer/src/app/services/app.settings.ts
+++ cms-designer/src/app/services/app.settings.ts
@@ -16,7 +16,7 @@ export class AppSettings implements EnvironmentSettings {
     previewTimeout = 120000;
     assetsPath = 'assets/pages';
 
-    moduleLocation = null;
+    moduleLocation = '/Modules/$(VirtoCommerce.PageBuilderModule)/Content/builder/';
     version = null;
 
     storeId = null;
