e1ec1c6f1d8334fe20015c0cddfabf82aaca4551
basilkot <basilkot@gmail.com>
2022-06-08 10:51:20 +0200

    add endpoints for templates in theme

--- src/VirtoCommerce.PageBuilderModule.Web/Controllers/Api/PageBuilderController.cs
+++ src/VirtoCommerce.PageBuilderModule.Web/Controllers/Api/PageBuilderController.cs
@@ -19,7 +19,8 @@ using VirtoCommerce.Platform.Core.Common;
 namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
 {
     [Route("api/pagebuilder")]
-    [Authorize]
+    // todo: temporary disable authorization
+    //[Authorize]
     public class PageBuilderController : Controller
     {
         private readonly IBlobContentStorageProviderFactory _blobContentStorageProviderFactory;
@@ -29,48 +30,68 @@ namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
         }
 
         [HttpGet]
+        [Route("template")]
+        public async Task<ActionResult> GetTemplate(string storeId, string path)
+        {
+            var templatePath = $"/default/{path}";
+            var basePath = GetContentBasePath(storeId);
+            var storageProvider = _blobContentStorageProviderFactory.CreateProvider(basePath);
+
+            if ((await storageProvider.GetBlobInfoAsync(templatePath)) != null)
+            {
+                var stream = await storageProvider.OpenReadAsync(templatePath);
+                return File(stream, MimeTypeResolver.ResolveContentType(templatePath));
+            }
+            return NotFound(new 
+            {
+                basePath = basePath, templatePath = templatePath
+            });
+        }
+
+        [HttpGet]
         [Route("templates")]
         public async Task<ActionResult> GetTemplates(string storeId)
         {
             var result = await GetFilesFromFolder(storeId, "templates");
-            return Ok(result);
+            return Content(result, "application/json");
         }
 
         [HttpGet]
         [Route("sections")]
-        public async Task<ActionResult> GetSections(string storeId)
+        public async Task<ActionResult> GetSectionsSettings(string storeId)
         {
-            var result = await GetFilesFromFolder(storeId, "sections");
-            return Ok(result);
+            var sections = await GetFilesFromFolder(storeId, "sections");
+            var blocks = await GetFilesFromFolder(storeId, "blocks");
+            return Content($"{{ \"sections\": {sections}, \"blocks\": {blocks} }}", "application/json");
         }
 
         [HttpGet]
-        [Route("blocks")]
-        public async Task<ActionResult> GetBlocks(string storeId)
+        [Route("objects")]
+        public async Task<ActionResult> GetObjects(string storeId)
         {
-            var result = await GetFilesFromFolder(storeId, "blocks");
+            var result = await GetFilesFromFolder(storeId, "objects");
             return Ok(result);
         }
 
         [HttpPost]
         [Route("save-template-draft")]
-        public async Task<ActionResult> SaveTemplateDraft(string storeId, [FromBody]TemplatesModel model)
+        public async Task<ActionResult> SaveTemplateDraft(string storeId, [FromBody] SaveFilesModel value)
         {
-            await SaveFilesTo(storeId, model, "/default/config/drafts");
+            await SaveFilesTo(storeId, value, "/default/config/drafts");
             return Ok();
         }
 
         [HttpPost]
         [Route("save")]
-        public async Task<ActionResult> SaveTemplates(string storeId, [FromBody] TemplatesModel model)
+        public async Task<ActionResult> SaveTemplates(string storeId, [FromBody] SaveFilesModel value)
         {
-            await SaveFilesTo(storeId, model, "/default/templates");
+            await SaveFilesTo(storeId, value, "/default");
             return Ok();
         }
 
-        private async Task SaveFilesTo(string storeId, TemplatesModel model, string path)
+        private async Task SaveFilesTo(string storeId, SaveFilesModel model, string path)
         {
-            var templates = JsonConvert.DeserializeObject<JObject>(model.Templates).ToObject<Dictionary<string, JObject>>();
+            var templates = JsonConvert.DeserializeObject<JObject>(model.Files).ToObject<Dictionary<string, JObject>>();
             var storageProvider = _blobContentStorageProviderFactory.CreateProvider(GetContentBasePath(storeId));
             var settings = new JsonSerializerSettings { Formatting = Formatting.Indented };
 
@@ -112,9 +133,9 @@ namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
             return result;
         }
 
-        public class TemplatesModel
+        public class SaveFilesModel
         {
-            public string Templates { get; set; }
+            public string Files { get; set; }
         }
     }
 }
