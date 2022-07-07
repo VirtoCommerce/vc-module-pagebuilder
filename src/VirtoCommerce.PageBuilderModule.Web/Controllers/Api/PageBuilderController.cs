0cec8c69867ac012a2f816e92d229c18a3c764a0
basilkot <basilkot@gmail.com>
2022-07-07 08:51:44 +0200

    api changes

--- src/VirtoCommerce.PageBuilderModule.Web/Controllers/Api/PageBuilderController.cs
+++ src/VirtoCommerce.PageBuilderModule.Web/Controllers/Api/PageBuilderController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using VirtoCommerce.AssetsModule.Core.Assets;
@@ -24,27 +25,35 @@ namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
     public class PageBuilderController : Controller
     {
         private readonly IBlobContentStorageProviderFactory _blobContentStorageProviderFactory;
-        public PageBuilderController(IBlobContentStorageProviderFactory blobContentStorageProviderFactory)
+        private readonly ContentOptions _options;
+
+        private const string _blogsFolderName = "blogs";
+        private const string _pages = "pages";
+        private const string _themes = "themes";
+
+        public PageBuilderController(
+            IBlobContentStorageProviderFactory blobContentStorageProviderFactory,
+            IOptions<ContentOptions> options)
         {
             _blobContentStorageProviderFactory = blobContentStorageProviderFactory;
+            _options = options.Value;
         }
 
         [HttpGet]
         [Route("template")]
-        public async Task<ActionResult> GetTemplate(string storeId, string path)
+        public async Task<ActionResult> GetTemplate(string storeId, string path, string type)
         {
-            var templatePath = $"/default/{path}";
-            var basePath = GetContentBasePath(storeId);
+            var basePath = GetContentBasePath(storeId, type);
             var storageProvider = _blobContentStorageProviderFactory.CreateProvider(basePath);
 
-            if ((await storageProvider.GetBlobInfoAsync(templatePath)) != null)
+            if ((await storageProvider.GetBlobInfoAsync(path)) != null)
             {
-                var stream = await storageProvider.OpenReadAsync(templatePath);
-                return File(stream, MimeTypeResolver.ResolveContentType(templatePath));
+                var stream = await storageProvider.OpenReadAsync(path);
+                return File(stream, MimeTypeResolver.ResolveContentType(path));
             }
             return NotFound(new 
             {
-                basePath = basePath, templatePath = templatePath
+                basePath = basePath, templatePath = path
             });
         }
 
@@ -52,7 +61,7 @@ namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
         [Route("templates")]
         public async Task<ActionResult> GetTemplates(string storeId)
         {
-            var result = await GetFilesFromFolder(storeId, "templates");
+            var result = await GetFilesFromFolder(storeId, "templates", "Themes");
             return Content(result, "application/json");
         }
 
@@ -60,8 +69,8 @@ namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
         [Route("sections")]
         public async Task<ActionResult> GetSectionsSettings(string storeId)
         {
-            var sections = await GetFilesFromFolder(storeId, "sections");
-            var blocks = await GetFilesFromFolder(storeId, "blocks");
+            var sections = await GetFilesFromFolder(storeId, "sections", "Themes");
+            var blocks = await GetFilesFromFolder(storeId, "blocks", "Themes");
             return Content($"{{ \"sections\": {sections}, \"blocks\": {blocks} }}", "application/json");
         }
 
@@ -69,36 +78,40 @@ namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
         [Route("objects")]
         public async Task<ActionResult> GetObjects(string storeId)
         {
-            var result = await GetFilesFromFolder(storeId, "objects");
+            var result = await GetFilesFromFolder(storeId, "objects", "Themes");
             return Ok(result);
         }
 
-        [HttpPost]
-        [Route("save-template-draft")]
-        public async Task<ActionResult> SaveTemplateDraft(string storeId, [FromBody] SaveFilesModel value)
-        {
-            await SaveFilesTo(storeId, value, "/default/config/drafts");
-            return Ok();
-        }
+        //[HttpPost]
+        //[Route("save-template-draft")]
+        //public async Task<ActionResult> SaveTemplateDraft(string storeId, [FromBody] SaveFilesModel value)
+        //{
+        //    await SaveFilesTo(storeId, value, "/default/config/drafts");
+        //    return Ok();
+        //}
 
         [HttpPost]
         [Route("save")]
         public async Task<ActionResult> SaveTemplates(string storeId, [FromBody] SaveFilesModel value)
         {
-            await SaveFilesTo(storeId, value, "/default");
+            await SaveFilesTo(storeId, value);
             return Ok();
         }
 
-        private async Task SaveFilesTo(string storeId, SaveFilesModel model, string path)
+        private async Task SaveFilesTo(string storeId, SaveFilesModel model)
         {
-            var templates = JsonConvert.DeserializeObject<JObject>(model.Files).ToObject<Dictionary<string, JObject>>();
-            var storageProvider = _blobContentStorageProviderFactory.CreateProvider(GetContentBasePath(storeId));
-            var settings = new JsonSerializerSettings { Formatting = Formatting.Indented };
+            var files = JsonConvert.DeserializeObject<IEnumerable<SaveFileModel>>(model.Files);
 
-            foreach (var templateName in templates.Keys)
+            var providers = new Dictionary<string, IBlobContentStorageProvider>();
+
+            var settings = new JsonSerializerSettings { Formatting = Formatting.Indented };
+            foreach (var file in files)
             {
-                var filepath = $"{path}/{templateName}";
-                var content = templates[templateName];
+                var storageProvider = providers.ContainsKey(file.Type)
+                    ? providers[file.Type]
+                    : (providers[file.Type] = _blobContentStorageProviderFactory.CreateProvider(GetContentBasePath(storeId, file.Type)));
+                var filepath = file.Path;
+                var content = file.Content;
                 await using var targetStream = await storageProvider.OpenWriteAsync(filepath);
                 await using var writer = new StreamWriter(targetStream);
                 var stringContent = JsonConvert.SerializeObject(content, settings);
@@ -106,10 +119,10 @@ namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
             }
         }
 
-        private async Task<string> GetFilesFromFolder(string storeId, string folder)
+        private async Task<string> GetFilesFromFolder(string storeId, string folder, string type)
         {
             var templatesFolder = $"/default/config/schemas/{folder}";
-            var basePath = GetContentBasePath(storeId);
+            var basePath = GetContentBasePath(storeId, type);
             var storageProvider = _blobContentStorageProviderFactory.CreateProvider(basePath);
             var files = await storageProvider.SearchAsync(templatesFolder, "*.json");
             var result = $"{{{string.Join(", ", files.Results.Select(file => $"\"{GetKey(file)}\": {GetContent(file, storageProvider)}"))}}}";
@@ -127,15 +140,44 @@ namespace VirtoCommerce.PageBuilderModule.Web.Controllers.Api
             return reader.ReadToEnd();
         }
 
-        private string GetContentBasePath(string storeId)
+        private string GetContentBasePath(string storeId, string contentType)
         {
-            var result = $"Themes/{storeId}";
-            return result;
+            //var result = $"{type}/{storeId}";
+            //return result;
+            if (_options.TypeMappings != null && _options.TypeMappings.Count() > 0 && _options.TypeMappings.ContainsKey(contentType))
+            {
+                var mapping = _options.TypeMappings[contentType];
+                return string.Join('/', mapping.Select(x => x == "_storeId" ? storeId : x));
+            }
+
+            var retVal = string.Empty;
+            if (contentType.EqualsInvariant(_themes))
+            {
+                retVal = "Themes/" + storeId;
+            }
+            else if (contentType.EqualsInvariant(_pages))
+            {
+                retVal = "Pages/" + storeId;
+            }
+            else if (contentType.EqualsInvariant(_blogsFolderName))
+            {
+                retVal = "Pages/" + storeId + $"/{_blogsFolderName}";
+            }
+
+            return retVal;
+
         }
 
         public class SaveFilesModel
         {
             public string Files { get; set; }
+        }
+
+        public class SaveFileModel
+        {
+            public string Path { get; set; }
+            public string Type { get; set; }
+            public JObject Content { get; set; }
         }
     }
 }
--- src/VirtoCommerce.PageBuilderModule.Web/Module.cs
+++ src/VirtoCommerce.PageBuilderModule.Web/Module.cs
@@ -1,28 +1,38 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.PageBuilderModule.Web
 {
-    public class Module : IModule
+    public class Module : IModule, IHasConfiguration
     {
         public ManifestModuleInfo ModuleInfo { get; set; }
+        public IConfiguration Configuration { get; set; }
 
         public void Initialize(IServiceCollection serviceCollection)
         {
-            // Method intentionally left empty.
+            // todo: remove it after release content module
+            serviceCollection.AddOptions<ContentOptions>().Bind(Configuration.GetSection("Content"));
         }
 
         public void PostInitialize(IApplicationBuilder appBuilder)
         {
             var settingsRegistrar = appBuilder.ApplicationServices.GetRequiredService<ISettingsRegistrar>();
-            settingsRegistrar.RegisterSettings(ModuleConstants.Settings.AllSettings, ModuleInfo.Id);
+            settingsRegistrar.RegisterSettings(ModuleConstants.Settings.AllSettings, ModuleInfo.Id);            
         }
 
         public void Uninstall()
         {
             // Method intentionally left empty.
         }
+    }
+
+    // remove it after release content module
+    public class ContentOptions
+    {
+        public IDictionary<string, string[]> TypeMappings { get; set; }
     }
 }
