4d7021b84293e0b7bc14b0875c6367f0f2e6d5d6
Basil Kotov <basilkot@gmail.com>
2025-08-05 16:25:44 +0100

    load groups without pages

--- src/VirtoCommerce.PageBuilderModule.Data/Repositories/PageBuilderModuleRepository.cs
+++ src/VirtoCommerce.PageBuilderModule.Data/Repositories/PageBuilderModuleRepository.cs
@@ -37,8 +37,8 @@ public class PageBuilderModuleRepository : DbContextRepositoryBase<PageBuilderMo
         }
 
         var groups = ids.Count == 1
-            ? await GroupedPageBuilderPages.Include(x => x.Pages).Where(x => x.Id == ids.First()).ToListAsync()
-            : await GroupedPageBuilderPages.Include(x => x.Pages).Where(x => ids.Contains(x.Id)).ToListAsync();
+            ? await GroupedPageBuilderPages.Where(x => x.Id == ids.First()).ToListAsync()
+            : await GroupedPageBuilderPages.Where(x => ids.Contains(x.Id)).ToListAsync();
 
         //if (groups.Count > 0)
         //{
