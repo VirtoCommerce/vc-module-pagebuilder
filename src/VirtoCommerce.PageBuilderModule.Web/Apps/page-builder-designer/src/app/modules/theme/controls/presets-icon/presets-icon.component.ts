7d9d21db631947e29da15bcd2b836e5b2e898c99
Basil Kotov <basilkot@gmail.com>
2026-03-20 20:43:02 +0000

    refactor: streamline presets icon component logic and enhance background image handling

--- src/VirtoCommerce.PageBuilderModule.Web/Apps/page-builder-designer/src/app/modules/theme/controls/presets-icon/presets-icon.component.ts
+++ src/VirtoCommerce.PageBuilderModule.Web/Apps/page-builder-designer/src/app/modules/theme/controls/presets-icon/presets-icon.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { NgStyle } from '@angular/common';
 import { IconWithPreviewComponent } from '@core/components/icon-with-preview/icon-with-preview.component';
 
@@ -11,29 +11,18 @@ import { IconWithPreviewComponent } from '@core/components/icon-with-preview/ico
 })
 export class PresetsIconComponent {
 
-    private _name!: string;
-    background!: string;
-    color!: string;
-    letter!: string;
-
+    readonly name = input.required<string>();
     readonly icon = input<string | null | undefined>(undefined);
     readonly preview = input<string | null | undefined>(undefined);
 
-    @Input({ required: true }) get name(): string {
-        return this._name;
-    }
-    set name(value: string) {
-        this._name = value;
-        this.background = this.stringToColour();
-        this.color = this.getTextColor();
-        this.letter = value[0];
-    }
+    readonly background = computed(() => this.stringToColour());
+    readonly color = computed(() => this.getTextColor());
+    readonly letter = computed(() => this.name()[0]);
 
     private stringToColour(): string {
-        // return "#000000";
         let hash = 0;
-        for (let i = 0; i < this.name.length; i++) {
-            hash = this.name.charCodeAt(i) + ((hash << 5) - hash);
+        for (let i = 0; i < this.name().length; i++) {
+            hash = this.name().charCodeAt(i) + ((hash << 5) - hash);
         }
         let colour = '#';
         for (let i = 0; i < 3; i++) {
@@ -44,12 +33,17 @@ export class PresetsIconComponent {
         return colour;
     }
 
+    getIconBackground(): string {
+        const url = encodeURI((this.icon() ?? this.preview()) ?? '').replace(/'/g, '%27');
+        return `url('${url}')`;
+    }
+
     private getTextColor(): string {
-        const hexcolor = this.background.substring(1);
+        const hexcolor = this.background().substring(1);
         const r = parseInt(hexcolor.substring(0, 2), 16);
-        const g = parseInt(hexcolor.substring(2, 2), 16);
-        const b = parseInt(hexcolor.substring(4, 2), 16);
+        const g = parseInt(hexcolor.substring(2, 4), 16);
+        const b = parseInt(hexcolor.substring(4, 6), 16);
         const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
         return (yiq >= 128) ? 'black' : 'white';
-    };
+    }
 }
