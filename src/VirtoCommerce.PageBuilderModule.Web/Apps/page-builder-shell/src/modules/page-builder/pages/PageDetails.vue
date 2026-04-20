4b85e79e4db1d3a0cc332df9373acf85e01a49ba
Basil Kotov <basilkot@gmail.com>
2026-04-20 15:10:42 +0100

    Add localized labels for Basic Information and Advanced Options in multiple languages

--- src/VirtoCommerce.PageBuilderModule.Web/Apps/page-builder-shell/src/modules/page-builder/pages/PageDetails.vue
+++ src/VirtoCommerce.PageBuilderModule.Web/Apps/page-builder-shell/src/modules/page-builder/pages/PageDetails.vue
@@ -16,7 +16,7 @@
         <PageStatus :item="item" />
 
         <VcCard
-          header="Basic information"
+          :header="$t('PAGE_BUILDER.PAGES.DETAILS.SECTIONS.BASIC_INFORMATION')"
           class="tw-p-4"
         >
           <VcCol class="tw-gap-4">
@@ -78,7 +78,7 @@
         </VcCard>
 
         <VcCard
-          header="Advanced options"
+          :header="$t('PAGE_BUILDER.PAGES.DETAILS.SECTIONS.ADVANCED_OPTIONS')"
           class="tw-p-4"
         >
           <VcCol class="tw-gap-4">
