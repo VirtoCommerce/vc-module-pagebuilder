1819325c8820c8199166da24d4d3f2db3d93f24e
Oleg Zhuk <zhukoo@gmail.com>
2026-06-23 16:46:12 +0200

    fix: PostgreSQL 42P01 in ContentStreamRepository raw SQL (#148)

--- src/VirtoCommerce.PageBuilderModule.Data.MySql/MySqlContentStreamRepository.cs
+++ src/VirtoCommerce.PageBuilderModule.Data.MySql/MySqlContentStreamRepository.cs
@@ -6,6 +6,12 @@ namespace VirtoCommerce.PageBuilderModule.Data.MySql;
 
 public class MySqlContentStreamRepository(PageBuilderModuleDbContext dbContext) : ContentStreamRepository(dbContext)
 {
+    protected override string QuoteOpen => "`";
+    protected override string QuoteClose => "`";
+
+    protected override string AppendContentChunkSql =>
+        $"UPDATE {Table} SET {ContentColumn} = CONCAT({ContentColumn}, @chunk) WHERE {IdColumn} = @id";
+
     protected override void SetIdParameter(DbCommand cmd, string value)
     {
         cmd.Parameters.Add(new MySqlParameter("@id", value));
